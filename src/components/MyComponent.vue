007c57b7555cee709a718c1422f99f21dc8a4805
Rendery Codes <48937882+Rendery@users.noreply.github.com>
2023-09-03 12:09:52 -0500

    Incio-Vue

---
+++ src/components/MyComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>My Component!</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      mensaje: 'hola',
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
